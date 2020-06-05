f1dfc5678c869e6bcfb520b5a74bddd82f3ac1eb
Cezar <gabriel.cezar.coder@gmail.com>
2020-06-05 20:16:42 -0300

    NLW SecondDayComplete Revised

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -19,11 +19,14 @@ function populateUFs(){
 populateUFs()
 
 function getCities(event){
-    const citySelect = document.querySelector("select[name=city]")
-
-    console.log(event.target.value)
+    const citySelect = document.querySelector("[name=city]")
+    const stateInput = document.querySelector("[name=state]")
 
     const ufValue = event.target.value
+
+    const indexOfSelectedState = event.target.selectedIndex
+    stateInput.value = event.target.options[indexOfSelectedState].text
+
 
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
 
