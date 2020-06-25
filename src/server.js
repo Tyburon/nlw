2bb27fe6a2c53851d5b001ee1fb4281f08fb26e8
Cezar <gabriel.cezar.coder@gmail.com>
2020-06-25 20:42:17 -0300

    NLW FourthDayComplete

--- src/server.js
+++ src/server.js
@@ -1,6 +1,39 @@
 const express = require("express")
 const server = express()
 
+
+//configurar pasta publica
+server.use(express.static("public"))
+
+
+//utiliizando template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+
+})
+
+
+
+
+//configurar caminhos da aplicação
+//pagina inicial
+server.get("/", function(req, res){
+    return res.render("index.html")
+
+})
+
+//criar ponto de coleta
+server.get("/create-point.html", function(req, res){
+    return res.render("create-point.html")
+
+})
+//resultados da pesquisa
+server.get("/search-results.html", function(req, res){
+    return res.render("search-results.html")
+
+})
 //start server
 
 server.listen(3000)
